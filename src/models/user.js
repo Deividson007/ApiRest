dc866561f295fc2047fba5b4decad2b30e19f9e1
Deividson Oliveira Santos <deividson.santos@progen.lan.br>
2019-10-15 15:37:21 -0300

    Inclusão do projeto

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password:  {
+        type: String,
+        required: true,
+        select: false
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
